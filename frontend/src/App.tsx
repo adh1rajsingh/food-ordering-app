265ed6d4776fcf3a9d5311f44840d4e9a648dd65
Adhiraj Singh <adhirajsingh@Adhirajs-MacBook-Air.local>
2024-12-13 10:07:40 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './index.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <Button>Click me</Button>
+     
+    
+  )
+}
+
+export default App
