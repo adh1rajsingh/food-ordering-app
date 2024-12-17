305c3960c95ee649db93c2dfd44e9833e40c4fca
Adhiraj Singh <adhirajsingh@Adhirajs-MacBook-Air.local>
2024-12-17 10:03:19 -0500

    Authenticated User Links

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import "dotenv/config"
-import mongoose from 'mongoose'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("connected to database"))
-const app = express()
-app.use(express.json())
-app.use(cors())
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database"));
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/test", async (req: Request, res: Response)=> {
-    res.json({message: "Test Successful"})
-})
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Test Successful" });
+});
 
-app.listen(3000, ()=> {console.log("listening on port 3000")})
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
