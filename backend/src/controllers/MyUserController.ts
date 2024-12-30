a976ad704350e27c2421237420e1f16e54d6a529
Adhiraj Singh <adhirajsingh@Adhirajs-MacBook-Air.local>
2024-12-30 11:20:06 -0500

    updated validation

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -2,10 +2,6 @@ import { Request, Response } from "express";
 import User from "../models/user";
 
 const createCurrentUser = async (req: Request, res: Response) => {
-  //1. check if the user exists
-  //2. create the user if it doesn't exist
-  //3. return the user object to the calling client
-
   try {
     const { auth0Id } = req.body;
     const existingUser = await User.findOne({ auth0Id });
@@ -26,6 +22,31 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findOne(req.userId);
+
+    if (!user) {
+       res.status(404).json({ message: "User not found" });
+       return;
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: " Error while updating user " });
+  }
+};
+
 export default {
   createCurrentUser,
+  updateCurrentUser,
 };
